3ee4e5a6bca0d912d6e204a7985a36291cd2b178
Vitalii Yatsenko <yatsik95@gmail.com>
2015-10-10 18:36:25 +0300

    Update Fibchi.cpp
    working function

--- Fibchi.cpp
+++ Fibchi.cpp
@@ -5,26 +5,39 @@
 #include <iostream>
 
 using namespace std;
+
+double fibonachi(int numb)// заголовок функции
+{
+    double rezult = 0; // инициализируем переменную rezult значением 1
+    rezult=((pow((1+sqrt(5.0))/2, numb)-pow((1-sqrt(5.0))/2, numb))/sqrt(5.0));
+    return (rezult);
+}
  
 int main(void)
 {
-	int i,n=1;
+	int i,n=0,k=1;
 	double a=1,b=2,x0,x1,x2,xx,delta,u,v;
 	double e=0.0001,f,fu,fv,fx=0,Fibchi[100];
-	
-	for (i=1;i<=100;i++)
-	{
-		Fibchi[i]=(pow((1+sqrt(5.0))/2, i)-pow((1-sqrt(5.0))/2, i))/sqrt(5.0);
+
+	do{
+	n++;
 	}
+	while ((b-a)/(fibonachi(n+2))>e);
+	cout<<"Count_Fibchi="<<n;
+	
+/*	for (i=1;i<=n;i++)
+	{
+		Fibchi[i]=((pow((1+sqrt(5.0))/2, i)-pow((1-sqrt(5.0))/2, i))/sqrt(5.0));
+	}*/
     
-    	u=a+((Fibchi[n]/Fibchi[n+2])*(b-a));
+    	u=a+((fibonachi(n)/fibonachi(n+2))*(b-a));
     	v=a+b-u;
 
 	fu=pow(u,4.0)+(4*pow(u,3.0))-(3*u*u)-(36*u)+45;
 	fv=pow(v,4.0)+(4*pow(v,3.0))-(3*v*v)-(36*v)+45;
 
 	//u=a+(((3-sqrt(5.0))/2)*(b-a));
-	for (i=1;i<=100;i++)
+	for (i=1;i<=n;i++)
 	{
 		if (fu<=fv){
 			b=v;
@@ -41,52 +54,16 @@ int main(void)
 			fv=pow(v,4.0)+(4*pow(v,3.0))-(3*v*v)-(36*v)+45;
 		}
 		if (u>v){
-			u=a+((Fibchi[n]/Fibchi[n+2])*(b-a));  //back to this step, have problem
+			u=a+((fibonachi(n-i+1)/fibonachi(n-i+3))*(b-a));  //back to this step, have problem
 			v=a+b-u;
 			fu=pow(u,4.0)+(4*pow(u,3.0))-(3*u*u)-(36*u)+45;
 			fv=pow(v,4.0)+(4*pow(v,3.0))-(3*v*v)-(36*v)+45;
 		}
 	}
-	xx=(a+b)*2
+	xx=(a+b)*2;
 	fx=pow(xx,4.0)+(4*pow(xx,3.0))-(3*xx*xx)-(36*xx)+45;
 	
-	
-	
-	
-	
-	/*
-	do
-		{	
-			if(fu<=fv)
-				{
-					b=v;
-					v=u;
-					fv=fu;
-					u=a+b-v;
-					fu=pow(u,4.0)+(4*pow(u,3.0))-(3*u*u)-(36*u)+45;
-				}
-			else 
-				{
-					a=u;
-					u=v;
-					fu=fv;
-					v=a+b-u;
-					fv=pow(v,4.0)+(4*pow(v,3.0))-(3*v*v)-(36*v)+45;
-				}
-			if (u>v) 
-				{
-					u=a+(((3-sqrt(5.0))/2)*(b-a));
-					v=a+b-u;
-					fu=pow(u,4.0)+(4*pow(u,3.0))-(3*u*u)-(36*u)+45;
-					fv=pow(v,4.0)+(4*pow(v,3.0))-(3*v*v)-(36*v)+45;
-				}
-		}
-			while (b-a>e);
-			xx=(a+b)/2;
-			fx=pow(xx,4.0)+(4*pow(xx,3.0))-(3*xx*xx)-(36*xx)+45;
-
-		*/
-		cout << xx<<"\t"<<fx<<"\n";
+		cout <<"\n"<< xx<<"\t"<<fx<<"\n";
 	
 	system("pause");  // by Vitalii Yatsenko
 }
